639f1b63fbb94c989b881799574508f548d2da67
Peijun/ページュン <contact.zouri.music@gmail.com>
2018-09-27 23:02:24 +0900

    トップページ仮実装

--- radiopro/radiohome/views.py
+++ radiopro/radiohome/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from radioupload.models import FileModel
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'radiohome/index.html')
+    data = FileModel.objects.all()
+    params = {
+        'data' : data,
+    }
+    return render(request, 'radiohome/index.html', params)
--- radiopro/radioupload/models.py
+++ radiopro/radioupload/models.py
@@ -10,3 +10,4 @@ class FileModel(models.Model):
     title = models.CharField(max_length=32, blank=False)
     description = models.CharField(max_length=255, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    
